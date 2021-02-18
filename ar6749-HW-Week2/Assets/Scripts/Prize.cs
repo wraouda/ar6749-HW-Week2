3ee598f0ce0259e3bb612f46d8e6cc4830696f8e
Walid <ar6749@nyu.edu>
2021-02-18 13:35:47 -0500

    Added scoring system

---
+++ ar6749-HW-Week2/Assets/Scripts/Prize.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
+
+
+public class Prize : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        transform.position = new Vector2(Random.Range(-5,5), Random.Range(-5,5));
+    }
+}
