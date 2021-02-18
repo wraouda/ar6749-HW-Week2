e2ffd515ba1637357174c26e48aebc67e5fd23c1
Walid <ar6749@nyu.edu>
2021-02-18 13:35:32 -0500

    Added prize and scene changes

--- ar6749-HW-Week2/Assets/Scripts/GameManager.cs
+++ ar6749-HW-Week2/Assets/Scripts/GameManager.cs
@@ -1,12 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager instance;
+
+    public int score = 0;
+    
+    private void Awake()
     {
+        if (instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
         
     }
 
